81b9e7ee377aabbafe30a0e41643c8bbb29b1b0c
DanRantino <danielaugustofurst@hotmail.com>
2021-07-24 00:26:01 -0300

    first commit

---
+++ src/main/java/bootcamp/dio/PersonsAPI/PersonsApiApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.dio.PersonsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/dio/PersonsAPI/PersonsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.dio.PersonsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
